2701a1cd1d9d4477a3fdaadde8c259edf1297216
pnikitits <112090831+pnikitits@users.noreply.github.com>
2024-09-21 12:24:26 +0200

    [REFAC] => rename __main__ file

--- __main__.py
+++ __main__.py
@@ -5,6 +5,10 @@ from .ui.split.chat_window import ChatApp
 
 
 if __name__ == '__main__':
+    """
+    Main entry point for the application.
+    Run `python -m iMessagesNet` to start the application.
+    """
     app = QApplication(sys.argv)
     chat_app = ChatApp()
     chat_app.show()
